4778167191d9dc23163638787ae458ac4a1b56ad
ADevKnight <adevknight@gmail.com>
2018-03-01 15:12:44 +0700

    154 Modifying GithubFollowersComponent

--- src/app/components/github-followers/github-followers.component.ts
+++ src/app/components/github-followers/github-followers.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GithubFollowersService } from '../../services/github-followers.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-github-followers',
   templateUrl: './github-followers.component.html',
-  styles: []
+  styles: [`
+    .media {
+      margin-bottom: 30px;
+    }
+  `]
 })
 export class GithubFollowersComponent implements OnInit {
+  followers: any[];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private service: GithubFollowersService) { }
 
   ngOnInit() {
+    Observable.combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap
+    ])
+    .switchMap(combined => {
+      const id = combined[0].get('id');
+      const page = combined[1].get('page');
+
+      return this.service.getFollowers();
+    })
+    .subscribe(followers => this.followers = followers);
   }
 
 }
