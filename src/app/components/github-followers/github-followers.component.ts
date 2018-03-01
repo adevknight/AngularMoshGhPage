89048d683fd20b00850ce4a016691555747c431e
ADevKnight <adevknight@gmail.com>
2018-03-01 14:30:58 +0700

    154 ng g c components/GithubFollowers -is --spec=false

---
+++ src/app/components/github-followers/github-followers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-github-followers',
+  templateUrl: './github-followers.component.html',
+  styles: []
+})
+export class GithubFollowersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
