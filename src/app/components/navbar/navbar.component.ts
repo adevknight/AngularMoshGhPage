f8e4a29eecac4904b0da878e1af03ce603bfd48f
ADevKnight <adevknight@gmail.com>
2018-03-02 11:18:56 +0700

    158 Different Component Configuration for Different Environment

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../../environments/environment';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class NavbarComponent implements OnInit {
+  navbarBgColor = environment.navbarBgColor;
 
   constructor() { }
 
